5ac2b41fe61bdeecf671a342d032b1152c736f41
mrajucha <youremail@example.com>
2025-04-20 23:56:10 +0530

    made minor changes

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_ecr_repository" "my_repo" {
-    name = "my_repo"
+    name = "my-repo"
     image_tag_mutability = "MUTABLE"
     image_scanning_configuration {
         scan_on_push = true
