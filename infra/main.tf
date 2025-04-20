344d5b565ebdf9448ba3e51c0b29b9fe88822bdd
mrajucha <youremail@example.com>
2025-04-20 23:33:02 +0530

    fixed main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -5,16 +5,16 @@ resource "aws_ecr_repository" "my_repo" {
     image_scanning_configuration {
         scan_on_push = true
     }
-    encryption_configuration {
-      encryption_type = AES256
-    }
+    # encryption_configuration {
+    #   encryption_type = AES256
+    # }
   
 }
 
 
 resource "aws_vpc" "demo_vpc" {
    cidr_block = "10.0.0.0/16"
-   ipv6_cidr_block = false
+   #ipv6_cidr_block = false
    
    tags = {
      name = "demo_vpc"
